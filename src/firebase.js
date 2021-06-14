692d2b3f8d021a52c3fdb509d01e974a07f41e6d
singhkong7 <singhpriyansh2014@gmail.com>
2021-06-14 11:43:01 +0530

    Added hooks and added firebase and firestore, allso provided the featoprovided feature to create from firestore database

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+const firebaseApp= firebase.initializeApp({
+    apiKey: "AIzaSyCRbC6pSQg3NuNrGoZj88QEPC7-C4n-zeM",
+  authDomain: "instagram-react-be57a.firebaseapp.com",
+  projectId: "instagram-react-be57a",
+  storageBucket: "instagram-react-be57a.appspot.com",
+  messagingSenderId: "328868155443",
+  appId: "1:328868155443:web:ef75c9322c6e8364a99346",
+  measurementId: "G-9ESKLMX8EG"
+    }
+  );
+  const db=firebaseApp.firestore();
+  const auth=firebase.auth();
+  const storage=firebase.storage();
+  export {auth,storage};
+  export default db;
+  export  {firebaseApp};
